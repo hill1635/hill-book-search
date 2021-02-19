ee1904e3b7a399f68572f2bdec439949f82b5e5a
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-02-19 13:16:05 -0700

    Added text, input and button elements and styling to SearchDiv.

--- client/src/components/SearchDiv/index.js
+++ client/src/components/SearchDiv/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import "./style.css";
 
 function SearchDiv() {
-    return (
-        <div>
-
-        </div>
-    );
+  return (
+    <div className="searchDiv col-11 mx-auto mt-5">
+      <h5>Book Search</h5>
+      <p>Book:</p>
+      <input className="col-12" type="text"></input>
+      <button>Search</button>
+    </div>
+  );
 }
 
 export default SearchDiv;
