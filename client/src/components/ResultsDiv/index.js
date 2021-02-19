4e7d3e04bce166b697ee81fa9c286a465a1204c1
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-02-19 13:06:37 -0700

    ResultsDiv styling.

--- client/src/components/ResultsDiv/index.js
+++ client/src/components/ResultsDiv/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./style.css";
 
 function ResultsDiv() {
-    return (
-        <div>
-
-        </div>
-    );
+  return (
+    <main className="col-11 mx-auto mt-4">
+      <h5>Search Results</h5>
+      <hr></hr>
+    </main>
+  );
 }
 
 export default ResultsDiv;
