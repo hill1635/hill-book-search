e69bd520265c36e8c23b1458e66d492d96dce786
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-02-19 12:57:22 -0700

    Navbar styling.

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark">
       <div className="container-fluid">
         <span className="navbar-brand">Google Book Search</span>
         <button
