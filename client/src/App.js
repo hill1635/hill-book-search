d32a1d56f48b5897cb96c34dc91cb85a844b3d79
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-02-19 11:48:04 -0700

    Changed component name.  Added basic fragment structure to Saved page.

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Saved from "./pages/Saved";
 import Search from "./pages/Search";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
       <div>
         <Navbar />
+        <Header />
         <Route exact path="/" component={Search} />
         <Route exact path="/saved" component={Saved} />
       </div>
