0f950ef71ecc2153912464ef9940798a2710d7d0
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-02-20 23:12:21 -0700

    Backend API routes created.

--- routes/api/books.js
+++ routes/api/books.js
@@ -2,5 +2,12 @@ const router = require("express").Router();
 const booksController = require("../../controllers/booksController");
 
 //POST, GET, and DELETE Routes
+router
+  .route("/")
+  .length(booksController.findAll)
+  .post(booksController.create);
+
+router.route("/:id")
+  .delete(booksController.remove);
 
 module.exports = router;
