340a6e142794c0b02819cb21b2b62b18d4ac67c4
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-02-20 23:36:13 -0700

    renderSaved function added to get books from db and set saved state to the response.  Saved state prop drilling with SavedDiv.

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import SavedDiv from "../components/SavedDiv";
 import Book from "../components/Book";
 import API from "../utils/API";
 
 function Saved() {
+  const [saved, setSaved] = useState();
+
+  function renderSaved() {
+    API.getBooks()
+      .then((res) => {
+        setSaved(res);
+        console.log("savedBooks: ", res);
+      })
+      .catch((err) => console.log(err));
+  }
+  renderSaved();
+  
   return (
     <div>
-      <SavedDiv />
+      <SavedDiv saved={saved} />
     </div>
   );
 }
