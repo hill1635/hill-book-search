226a8c92c33b569ec3e2863100825dc3b42cb6f8
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-02-20 23:21:10 -0700

    Frontend API calls added to get, add, and delete bdatabase books.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -6,4 +6,13 @@ export default {
       "https://www.googleapis.com/books/v1/volumes?q=" + search
     );
   },
+  getBooks: function() {
+    return axios.get("/api/books");
+  },
+  saveBook: function(bookData) {
+    return axios.post("/api/posts", bookData);
+  },
+  deleteBook: function(id) {
+    return axios.delete("/api/posts/" + id);
+  }
 };
