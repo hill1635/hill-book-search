d4768afdc13ca4bf2214a61fff0b50975099f5f7
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-06-01 17:19:55 -0600

    Book card styling.

--- client/src/components/Book/index.js
+++ client/src/components/Book/index.js
@@ -26,14 +26,22 @@ function Book(props) {
   return (
     <div>
       {bookList.map((book) => (
-        <div className="bookCard d-grid g-1 col-lg-4 col-md-5 col-xs-12 p-2 border border-solid rounded text-center d-inline-block">
-          <img src={book.smImg} alt={book.title}></img>
-          <h5>{book.title}</h5>
-          <p>{book.author}</p>
-          <p>Rating: {book.rating}</p>
-          <div className="row col-12">
-            <ViewBtn bookInfo={book} />
-            <SaveBtn bookInfo={book} />
+        <div className="bookCard card m-5 p-0 col-lg-4 col-md-5 col-xs-12 border border-solid rounded d-inline-block">
+          <div className="row g-0">
+            <div className="col-3">
+              <img src={book.lgImg} alt={book.title} className="ml-0"></img>
+            </div>
+            <div className="col-9">
+              <div className="card-body ml-4 p-0">
+                <h5>{book.title}</h5>
+                <p>{book.author}</p>
+                <p>Rating: {book.rating}</p>
+                <div className="buttons row col-12">
+                  <ViewBtn bookInfo={book} />
+                  <SaveBtn bookInfo={book} />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       ))}
