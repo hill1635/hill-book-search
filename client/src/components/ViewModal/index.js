c0607605c8b040f14a98d112bc98cc5664a0c75e
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-06-02 19:22:11 -0600

    Modal moved into own component.

---
+++ client/src/components/ViewModal/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./style.css";
+
+function ViewModal(props) {
+    var book = props.bookInfo;
+
+    return (
+        <div className="modal fade" id="modal" tabIndex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+            <div className= "modal-dialog">
+                <div className="modal-content">
+                <div className="modal-header">
+                    <h5 className="modal-title" id="modalLabel">
+                    {book.title}
+                    </h5>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div className="modal-body">
+                    <div className="row">
+                    <img className="d-inline" src={book.smImg} alt={book.title}></img>
+                    <p className="d-inline">Written by {book.author}</p>
+                    </div>
+                    <p>{book.synopsis}</p>
+                </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ViewModal;
