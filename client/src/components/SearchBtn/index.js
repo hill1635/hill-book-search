ac0ef2d58687e774eb6a53ecfc24d5f17d9ab5ce
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-06-02 22:05:04 -0600

    Search input and button styling.

--- client/src/components/SearchBtn/index.js
+++ client/src/components/SearchBtn/index.js
@@ -3,7 +3,9 @@ import "./style.css";
 
 function SearchBtn(props) {
   return (
-      <button className="searchBtn rounded btn-primary ml-1 float-right" onClick={props.onClick}>Search</button>
+      <button className="searchBtn col-1 border border-left-0 border-solid bg-transparent" onClick={props.onClick}>
+        <i class="fas fa-search text-primary"></i>
+      </button>
   );
 }
 
--- client/src/components/SearchDiv/index.js
+++ client/src/components/SearchDiv/index.js
@@ -4,13 +4,11 @@ import "./style.css";
 
 function SearchDiv(props) {
   return (
-    <div className="searchDiv col-11 mx-auto mt-5 p-3 rounded">
-      <h5>Book Search</h5>
-      <input className="searchInput col-11 rounded" type="text" onChange={props.onChange}></input>
-      <SearchBtn 
-      onClick={props.onClick}
-      search={props.search}
-      />
+    <div className="searchDiv col-11 mx-auto mt-5 p-3 text-center">
+      <div className="searchRow mx-auto col-6">
+        <input className="searchInput col-10 border border-right-0 border-solid" type="text" onChange={props.onChange}></input>
+        <SearchBtn onClick={props.onClick} search={props.search}/>
+      </div>
     </div>
   );
 }
